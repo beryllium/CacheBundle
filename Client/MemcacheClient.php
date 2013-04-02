f4a76abb4ec0eaa9a584f9abeb7db27708500ede
Jeremy Livingston <jeremyjlivingston@gmail.com>
2013-04-02 17:51:58 -0400

    Update formatting to comply with Symfony2 standards

--- Client/MemcacheClient.php
+++ Client/MemcacheClient.php
@@ -15,190 +15,174 @@ use Beryllium\CacheBundle\CacheClientInterface;
  */
 class MemcacheClient implements CacheClientInterface
 {
-	protected $safe = false;
-	protected $mem = null;
-	protected $servers = array();
-	protected $sockttl = 0.2;
-	protected $compression = false;
+    protected $safe = false;
+    protected $mem = null;
+    protected $servers = array();
+    protected $sockttl = 0.2;
+    protected $compression = false;
 
-	/**
-   * Constructs the cache client using an injected Memcache instance
-	 *
-	 * @access public
-	 * @return void
-	 */
-	public function __construct( \Memcache $memcache )
-	{
+    /**
+     * Constructs the cache client using an injected Memcache instance
+     *
+     * @access public
+     */
+    public function __construct(\Memcache $memcache)
+    {
         $this->mem = $memcache;
-	}
-
-	/**
-	 * Add a server to the memcache pool.
-	 *
-	 * Does not probe server, does not set Safe to true.
-	 *
-	 * Should really be private, or modified to handle the probeServer action itself.
-	 *
-	 * @param string $ip Location of memcache server
-	 * @param int $port Optional: Port number (default: 11211)
-	 * @access public
-	 * @return void
-	 */
-	public function addServer( $ip, $port = 11211 )
-	{
-		if ( is_object( $this->mem ) )
-		{
-			return $this->mem->addServer( $ip, $port );
-		}
-	}
-
-	/**
-	 * Add an array of servers to the memcache pool
-	 *
-	 * Uses ProbeServer to verify that the connection is valid.
-	 *
-	 * Format of array:
-	 *
-	 *   $servers[ '127.0.0.1' ] = 11211;
-	 *
-	 * Logic is somewhat flawed, of course, because it wouldn't let you add multiple
-	 * servers on the same IP.
-	 *
-	 * Serious flaw, right? ;-)
-	 *
-	 * @param array $servers See above format definition
-	 * @access public
-	 * @return void
-	 */
-	public function addServers( array $servers )
-	{
-		if ( count( $servers ) == 0 )
-		{
-			return false;
-		}
-
-		foreach ( $servers as $ip=>$port )
-		{
-			if ( intval( $port ) == 0 )
-			{
-				$port = NULL;
-			}
-			if ( $this->probeServer( $ip, $port ) )
-			{
-				$status = $this->addServer( $ip, $port );
-				$this->safe = true;
-			}
-		}
-	}
-
-	/**
-	 * Spend a few tenths of a second opening a socket to the requested IP and port
-	 *
-	 * The purpose of this is to verify that the server exists before trying to add it,
-	 * to cut down on weird errors when doing ->get(). This could be a controversial or
-	 * flawed way to go about this.
-	 *
-	 * @param string $ip IP address (or hostname, possibly)
-	 * @param int $port Port that memcache is running on
-	 * @access public
-	 * @return boolean True if the socket opens successfully, or false if it fails
-	 */
-	public function probeServer( $ip, $port )
-	{
-		$errno = null;
-		$errstr = null;
-		$fp = @fsockopen( $ip, $port, $errno, $errstr, $this->sockttl );
-
-		if ( $fp )
-		{
-			fclose( $fp );
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
-
-	/**
-	 * Retrieve a value from memcache
-	 * 
-	 * @param string|array $key Unique identifier or array of identifiers
-	 * @access public
-	 * @return mixed Requested value, or false if an error occurs
-	 */
-	public function get( $key )
-	{
-		if ( $this->isSafe() )
-		{
-			return $this->mem->get( $key );
-		}
-		else
-		{
-			return false;
-		}
-	}
-
-	/**
-	 * Add a value to the memcache
-	 *
-	 * @param string $key Unique key
-	 * @param mixed $value A value. I recommend a string, be it serialized or not - other values haven't been tested :)
-	 * @param int $ttl Number of seconds for the value to be valid for
-	 * @access public
-	 * @return void
-	 */
-	public function set( $key, $value, $ttl )
-	{
-		if ( $this->isSafe() )
-		{
-			return $this->mem->set( $key, $value, $this->compression, $ttl );
-		}
-		else
-		{
-			return false;
-		}
-	}
-
-  /**
-   * Delete a value from the memcache
-   * 
-   * @param string $key Unique key
-   * @access public
-   * @return void
-   */
-  public function delete( $key )
-  {
-    if ( $this->isSafe() )
-    {
-      return $this->mem->delete( $key, 0 );
     }
-    else
+
+    /**
+     * Add a server to the memcache pool.
+     *
+     * Does not probe server, does not set Safe to true.
+     *
+     * Should really be private, or modified to handle the probeServer action itself.
+     *
+     * @param string $ip Location of memcache server
+     * @param int $port Optional: Port number (default: 11211)
+     * @access public
+     * @return void
+     */
+    public function addServer($ip, $port = 11211)
     {
-      return false;
+        if (is_object($this->mem)) {
+            return $this->mem->addServer($ip, $port);
+        }
     }
-  }
 
-	/**
-	 * Check if the cache is live
-	 *
-	 * @access public
-	 * @return True if a valid server has been added, otherwise false
-	 */
-	public function isSafe()
-	{
-		return $this->safe;
-	}
+    /**
+     * Add an array of servers to the memcache pool
+     *
+     * Uses ProbeServer to verify that the connection is valid.
+     *
+     * Format of array:
+     *
+     *   $servers[ '127.0.0.1' ] = 11211;
+     *
+     * Logic is somewhat flawed, of course, because it wouldn't let you add multiple
+     * servers on the same IP.
+     *
+     * Serious flaw, right? ;-)
+     *
+     * @param array $servers See above format definition
+     * @access public
+     * @return void
+     */
+    public function addServers(array $servers)
+    {
+        if (count($servers) == 0) {
+            return false;
+        }
 
-  /**
-   * getStats returns the result of Memcache::getExtendedStats(), an associative array
-   * containing arrays of server stats
-   *
-   * @access public
-   * @return array Server stats array
-   */
-  public function getStats()
-  {
-    return $this->mem->getExtendedStats();
-  }
+        foreach ($servers as $ip => $port) {
+            if (intval($port) == 0) {
+                $port = null;
+            }
+
+            if ($this->probeServer($ip, $port)) {
+                $status = $this->addServer($ip, $port);
+                $this->safe = true;
+            }
+        }
+    }
+
+    /**
+     * Spend a few tenths of a second opening a socket to the requested IP and port
+     *
+     * The purpose of this is to verify that the server exists before trying to add it,
+     * to cut down on weird errors when doing ->get(). This could be a controversial or
+     * flawed way to go about this.
+     *
+     * @param string $ip IP address (or hostname, possibly)
+     * @param int $port Port that memcache is running on
+     * @access public
+     * @return boolean True if the socket opens successfully, or false if it fails
+     */
+    public function probeServer($ip, $port)
+    {
+        $errno = null;
+        $errstr = null;
+        $fp = @fsockopen($ip, $port, $errno, $errstr, $this->sockttl);
+
+        if ($fp) {
+            fclose($fp);
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Retrieve a value from memcache
+     *
+     * @param string|array $key Unique identifier or array of identifiers
+     * @access public
+     * @return mixed Requested value, or false if an error occurs
+     */
+    public function get($key)
+    {
+        if ($this->isSafe()) {
+            return $this->mem->get($key);
+        }
+
+        return false;
+    }
+
+    /**
+     * Add a value to the memcache
+     *
+     * @param string $key Unique key
+     * @param mixed $value A value. I recommend a string, be it serialized or not - other values haven't been tested :)
+     * @param int $ttl Number of seconds for the value to be valid for
+     * @access public
+     * @return void
+     */
+    public function set($key, $value, $ttl)
+    {
+        if ($this->isSafe()) {
+            return $this->mem->set($key, $value, $this->compression, $ttl);
+        }
+
+        return false;
+    }
+
+    /**
+     * Delete a value from the memcache
+     *
+     * @param string $key Unique key
+     * @access public
+     * @return void
+     */
+    public function delete($key)
+    {
+        if ($this->isSafe()) {
+            return $this->mem->delete($key, 0);
+        }
+
+        return false;
+    }
+
+    /**
+     * Check if the cache is live
+     *
+     * @access public
+     * @return True if a valid server has been added, otherwise false
+     */
+    public function isSafe()
+    {
+        return $this->safe;
+    }
+
+    /**
+     * getStats returns the result of Memcache::getExtendedStats(), an associative array
+     * containing arrays of server stats
+     *
+     * @access public
+     * @return array Server stats array
+     */
+    public function getStats()
+    {
+        return $this->mem->getExtendedStats();
+    }
 }
--- Command/CacheClientStatsCommand.php
+++ Command/CacheClientStatsCommand.php
@@ -10,146 +10,150 @@ use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 
 /**
  * Provides a command-line interface for viewing cache client stats
- * 
- * @author Kevin Boyd <beryllium@beryllium.ca> 
+ *
+ * @author Kevin Boyd <beryllium@beryllium.ca>
  * @license See LICENSE.md
  */
 class CacheClientStatsCommand extends ContainerAwareCommand
 {
+    /**
+     * Configure the CLI task
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        $this->setName('cacheclient:stats')
+             ->setDescription('Display Cache Statistics')
+             ->addOption('debug', 'd', InputOption::VALUE_NONE, 'Show debugging information');
+    }
 
-   /**
-    * Configure the CLI task
-    *
-    * @return void
-    */
-   protected function configure()
-   {
-      $this
-        ->setName('cacheclient:stats')
-        ->setDescription('Display Cache Statistics')
-        ->addOption('debug', 'd', InputOption::VALUE_NONE, 'Show debugging information')
-        ;
-   }
+    /**
+     * Execute the CLI task
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $debug = $input->getOption('debug');
+        $client = $this->getContainer()->get('beryllium_cache.client');
 
-   /**
-    * Execute the CLI task
-    *
-    * @param InputInterface $input
-    * @param OutputInterface $output
-    * @return void
-    */
-   protected function execute(InputInterface $input, OutputInterface $output)
-   {
-     $debug  = $input->getOption( 'debug' );
-     $client = $this->getContainer()->get( 'beryllium_cache.client' );
+        if (is_object($client) && method_exists($client, 'getStats')) {
+            $text = $this->formatStats($client->getStats(), $debug);
+        } else {
+            $text = 'Active cache client does not have a stats function.';
+        }
 
-     if ( is_object( $client ) && method_exists( $client, 'getStats' ) )
-     {
-       $text = $this->formatStats( $client->getStats(), $debug );
-     }
-     else
-     {
-       $text = 'Active cache client does not have a stats function.';
-     }
+        $output->writeln($text);
+    }
 
-     $output->writeln($text);
-   }
+    /**
+     * Format the raw array for the command line report
+     *
+     * @param array $stats An array of memcache::extendedstats
+     * @param bool $debug (default: false) True causes a raw dump of the stats array to be provided
+     * @return string ConsoleComponent-formatted output, suitable for ->writeln() usage
+     */
+    public function formatStats($stats, $debug = false)
+    {
+        if (!is_array($stats)) {
+            return "No statistics returned.\n";
+        }
 
-   /**
-    * Format the raw array for the command line report
-    * 
-    * @param array $stats An array of memcache::extendedstats 
-    * @param bool $debug (default: false) True causes a raw dump of the stats array to be provided
-    * @return string ConsoleComponent-formatted output, suitable for ->writeln() usage
-    */
-   public function formatStats( $stats, $debug = false )
-   {
-     if ( !is_array( $stats ) )
-     {
-       return "No statistics returned.\n";
-     }
+        if (count($stats) == 0) {
+            return "No statistics returned.\n";
+        }
 
-     if ( count( $stats ) == 0 )
-     {
-       return "No statistics returned.\n";
-     }
+        $out = "Servers found: " . count($stats) . "\n\n";
+        foreach ($stats as $host => $item) {
+            if (!is_array($item) || count($item) == 0) {
+                $out .= "  <error>" . $host . "</error>\n";
 
-     $out = "Servers found: " . count( $stats ) . "\n\n";
-     foreach( $stats as $host=>$item )
-     {
-       if ( !is_array( $item ) || count( $item ) == 0 )
-       {
-         $out .= "  <error>" . $host . "</error>\n";
+                continue;
+            }
 
-         continue;
-       }
+            $out .= "<info>Host:\t" . $host . "</info>\n";
 
-       $out .= "<info>Host:\t" . $host . "</info>\n";
+            $out .= "\tUsage: " . $this->formatUsage($item['bytes'], $item['limit_maxbytes']) . "\n";
+            $out .= "\tUptime: " . $this->formatUptime($item['uptime']) . "\n";
+            $out .= "\tOpen Connections: " . $item['curr_connections'] . "\n";
+            $out .= "\tHits: " . $item['get_hits'] . "\n";
+            $out .= "\tMisses: " . $item['get_misses'] . "\n";
+            if ($item['get_hits'] + $item['get_misses'] > 0) {
+                $out .= "\tHelpfulness: " . round(
+                    $item['get_hits'] / ($item['get_hits'] + $item['get_misses']) * 100,
+                    2
+                ) . "%\n";
+            }
 
-       $out .= "\tUsage: " . $this->formatUsage( $item[ 'bytes' ], $item[ 'limit_maxbytes' ] ) . "\n";
-       $out .= "\tUptime: " . $this->formatUptime( $item[ 'uptime' ] ) . "\n";
-       $out .= "\tOpen Connections: " . $item[ 'curr_connections' ] . "\n";
-       $out .= "\tHits: " . $item[ 'get_hits' ] . "\n";
-       $out .= "\tMisses: " . $item[ 'get_misses' ] . "\n";
-       if ( $item[ 'get_hits' ] + $item[ 'get_misses' ] > 0 )
-         $out .= "\tHelpfulness: " . round( $item[ 'get_hits' ] / ( $item[ 'get_hits' ] + $item[ 'get_misses' ] ) * 100, 2 ) . "%\n";
+            if ($debug) {
+                $out .= "\n";
+                foreach ($item as $key => $value) {
+                    $out .= "\t" . $key . ': ' . $value . "\n";
+                }
+            }
+        }
 
-       if ( $debug )
-       {
-         $out .= "\n";
-         foreach( $item as $key => $value )
-         {
-           $out .= "\t" . $key . ': ' . $value . "\n";
-         } 
-       }
-     }
+        return $out;
+    }
 
-     return $out;
-   }
+    /**
+     * Format the usage stats
+     *
+     * @param integer $bytes Cache usage (in bytes)
+     * @param integer $maxbytes Cache maximum size (in bytes)
+     * @return string A short string with friendly formatting
+     */
+    public function formatUsage($bytes, $maxbytes)
+    {
+        if (!is_numeric($maxbytes) || $maxbytes < 1) {
+            return '(undefined)';
+        }
 
-   /**
-    * Format the usage stats
-    * 
-    * @param integer $bytes Cache usage (in bytes)
-    * @param integer $maxbytes Cache maximum size (in bytes)
-    * @return string A short string with friendly formatting
-    */
-   public function formatUsage( $bytes, $maxbytes )
-   {
-     if ( !is_numeric( $maxbytes ) || $maxbytes < 1 )
-     {
-       return '(undefined)';
-     }
+        $out = round($bytes / $maxbytes, 3) . "% (";
+        $out .= round($bytes / 1024 / 1024, 2) . 'MB of ';
+        $out .= round($maxbytes / 1024 / 1024, 2) . 'MB)';
 
-     $out  = round( $bytes / $maxbytes, 3 ) . "% (";
-     $out .= round( $bytes / 1024 / 1024, 2 ) . 'MB of ';
-     $out .= round( $maxbytes / 1024 / 1024, 2 ) . 'MB)';
+        return $out;
+    }
 
-     return $out;
-   }
+    /**
+     * Formats the uptime to be friendlier
+     *
+     * @param integer $uptime Cache server uptime (in seconds)
+     * @return string A short string with friendly formatting
+     */
+    public function formatUptime($uptime)
+    {
+        $days = floor($uptime / 24 / 60 / 60);
+        $days_remainder = $uptime - ($days * 24 * 60 * 60);
+        $hours = floor($days_remainder / 60 / 60);
+        $hours_remainder = $days_remainder - ($hours * 60 * 60);
+        $minutes = floor($hours_remainder / 60);
+        $minutes_remainder = $hours_remainder - ($minutes * 60);
+        $seconds = $minutes_remainder;
+        $out = $uptime . ' seconds (';
 
-   /**
-    * Formats the uptime to be friendlier
-    * 
-    * @param integer $uptime Cache server uptime (in seconds)
-    * @return string A short string with friendly formatting
-    */
-   public function formatUptime( $uptime )
-   {
-     $days = floor( $uptime / 24 / 60 / 60 );
-     $days_remainder = $uptime - ( $days * 24 * 60 * 60 );
-     $hours = floor( $days_remainder / 60 / 60 );
-     $hours_remainder = $days_remainder - ( $hours * 60 * 60 );
-     $minutes = floor( $hours_remainder / 60 );
-     $minutes_remainder = $hours_remainder - ( $minutes * 60 );
-     $seconds = $minutes_remainder;
+        if ($days > 0) {
+            $out .= $days . ' days, ';
+        }
 
-     $out = $uptime . ' seconds (';
-     if ( $days > 0 )    $out .= $days . ' days, ';
-     if ( $hours > 0 )   $out .= $hours . ' hours, ';
-     if ( $minutes > 0 ) $out .= $minutes . ' minutes, ';
-     if ( $seconds > 0 ) $out .= $seconds . ' seconds';
+        if ($hours > 0) {
+            $out .= $hours . ' hours, ';
+        }
 
-     return $out . ')';
-   }
+
+        if ($minutes > 0) {
+            $out .= $minutes . ' minutes, ';
+        }
+
+
+        if ($seconds > 0) {
+            $out .= $seconds . ' seconds';
+        }
+
+        return $out . ')';
+    }
 }
